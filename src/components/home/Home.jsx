f8e59e5190e04faf237b8ff2c4b45febc8e3ebc6
Samson Cheung <samsonc89@yahoo.com>
2023-07-06 12:53:50 -0700

    Adjust styling of home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
+import "./home.css";
+
 const Home = () => {
   return (
     <div id="home">
       <h1>Hello, I'm Samson</h1>
       <h2>I'm a restaurant owner turned frontend web developer.</h2>
       <h2>
-        Read more
+        Read more{" "}
         <span className="highlight-container highlight-about">
           <span className="highlight">
             <a href="#about"> about me </a>
           </span>
-        </span>
-        or
+        </span>{" "}
+        or{" "}
         <span className="highlight-container highlight-work">
           <span className="highlight">
             <a href="#projects"> see my work.</a>
