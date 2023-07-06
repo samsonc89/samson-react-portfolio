698f71f6369dc173b4d42d61c76a3d0fe1162521
Samson Cheung <samsonc89@yahoo.com>
2023-07-06 12:10:45 -0700

    Add id to toggle button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <NavLinks />
-      <button>Toggle</button>
+      <button id="btn--theme-toggle">Toggle</button>
     </div>
   );
 };
