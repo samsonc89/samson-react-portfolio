7b9ef7cb22b5ee327f0d3bee7fb4fb0b351e43f4
Samson Cheung <samsonc89@yahoo.com>
2023-07-07 14:45:44 -0700

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEDVtVEfJkUyDBy0AFODu95Uof_wfaNiM",
+  authDomain: "samson-portfolio-225bc.firebaseapp.com",
+  projectId: "samson-portfolio-225bc",
+  storageBucket: "samson-portfolio-225bc.appspot.com",
+  messagingSenderId: "145650826269",
+  appId: "1:145650826269:web:eb749f00a5e84130d99af4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
