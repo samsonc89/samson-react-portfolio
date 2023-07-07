f218e7311085dec4e42f2ec2f05105221b8cfcfe
Samson Cheung <samsonc89@yahoo.com>
2023-07-07 11:33:06 -0700

    Add skill icons

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./skills.css";
+import HtmlIcon from "../../assets/HtmlIcon";
+import CssIcon from "../../assets/CssIcon";
+import JavascriptIcon from "../../assets/JavascriptIcon";
+import ReactIcon from "../../assets/ReactIcon";
 
 const Skills = () => {
   return (
     <div className="section-container" id="skills">
       <h1 className="section-heading">Skills</h1>
+      <div className="skills-container">
+        <HtmlIcon />
+        <CssIcon />
+        <JavascriptIcon />
+        <ReactIcon />
+      </div>
     </div>
   );
 };
