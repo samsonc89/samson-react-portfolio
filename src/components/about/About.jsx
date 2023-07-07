9bd9830d40ecd849b91c6e9e47ca4a8e8ec3d206
Samson Cheung <samsonc89@yahoo.com>
2023-07-07 11:45:37 -0700

    Fix classname

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,6 +5,32 @@ const About = () => {
   return (
     <div className="section-container" id="about">
       <h1 className="section-heading">About</h1>
+      <div className="about-content">
+        <p className="about-text">
+          Hi there! I'm a seasoned restaurant owner with a passion for
+          innovation and a desire to constantly evolve. After years of managing
+          my own successful restaurants, I'm excited to dive into the world of
+          frontend development and bring my unique perspective to this field.
+        </p>
+        <p className="about-text">
+          With a strong background in customer service and a love for
+          technology, I believe I have the skills and drive to excel in this new
+          career path. Over the past year I have been teaching myself HTML, CSS,
+          and JavaScript and am eager to gain hands-on experience through
+          real-world projects.
+        </p>
+        <p className="about-text">
+          My passion for creating a seamless and enjoyable experience for
+          customers has always been at the forefront of my work in the
+          restaurant industry. I aim to bring this same level of dedication and
+          attention to detail to my new role as a frontend developer.
+        </p>
+        <p className="about-text">
+          I'm excited to take on new challenges and be part of a team that works
+          to create amazing user experiences and finds solutions to complex
+          problems.
+        </p>
+      </div>
     </div>
   );
 };
