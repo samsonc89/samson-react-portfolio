ca54610a20ce40f873ceb4e634d9201b4547b1ba
Samson Cheung <samsonc89@yahoo.com>
2023-07-07 10:41:11 -0700

    Add github and live site icon

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 import "./projects.css";
+import GitIcon from "../../assets/GitIcon";
+import LiveIcon from "../../assets/LiveIcon.jsx";
 
 const ProjectCard = ({ projectData: project }) => {
   return (
@@ -9,15 +11,15 @@ const ProjectCard = ({ projectData: project }) => {
       </div>
       <h2 className="project-title">{project.title}</h2>
       <p className="project-description">{project.description}</p>
-      <div className="icon-wrapper">
-        <div className="git-wrapper">
+      <div className="icon-container">
+        <div className="icon-wrapper">
           <a href={project.github} target="_blank" rel="noreferrer">
-            {/* <GitIcon /> */}
+            <GitIcon />
           </a>
         </div>
-        <div className="live-wrapper">
+        <div className="icon-wrapper">
           <a href={project.live} target="_blank" rel="noreferrer">
-            {/* <LiveIcon /> */}
+            <LiveIcon />
           </a>
         </div>
       </div>
