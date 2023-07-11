51442e0aaaf33a675db3786be9d70fa047fee5b8
Samson Cheung <samsonc89@yahoo.com>
2023-07-10 19:36:49 -0700

    Add div to create angle effect

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,24 +3,27 @@ import "./home.css";
 
 const Home = () => {
   return (
-    <div className="section-container" id="home">
-      <h1 className="section-heading">Hello, I'm Samson.</h1>
-      <h2>I'm a restaurant owner turned frontend web developer.</h2>
-      <h2>
-        Read more{" "}
-        <span className="highlight-container highlight-about">
-          <span className="highlight">
-            <a href="#about"> about me </a>
+    <>
+      <div className="section-container" id="home">
+        <h1 className="section-heading">Hello, I'm Samson.</h1>
+        <h2>I'm a restaurant owner turned frontend web developer.</h2>
+        <h2>
+          Read more{" "}
+          <span className="highlight-container highlight-about">
+            <span className="highlight">
+              <a href="#about"> about me </a>
+            </span>
+          </span>{" "}
+          or{" "}
+          <span className="highlight-container highlight-work">
+            <span className="highlight">
+              <a href="#projects"> see my work.</a>
+            </span>
           </span>
-        </span>{" "}
-        or{" "}
-        <span className="highlight-container highlight-work">
-          <span className="highlight">
-            <a href="#projects"> see my work.</a>
-          </span>
-        </span>
-      </h2>
-    </div>
+        </h2>
+      </div>
+      <div className="home-angle"></div>
+    </>
   );
 };
 
