72af52700119642cbb9588c9fa133f7213cdd918
Samson Cheung <samsonc89@yahoo.com>
2023-07-10 20:29:48 -0700

    Add names to skills icons

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -10,10 +10,10 @@ const Skills = () => {
     <div className="section-container" id="skills">
       <h1 className="section-heading">Skills & Tools</h1>
       <div className="skills-container">
-        <HtmlIcon />
-        <CssIcon />
-        <JavascriptIcon />
-        <ReactIcon />
+        <HtmlIcon name={"HTML5"} />
+        <CssIcon name={"CSS3"} />
+        <JavascriptIcon name={"JavaScript"} />
+        <ReactIcon name={"React"} />
       </div>
       <div className="skills-angle"></div>
     </div>
