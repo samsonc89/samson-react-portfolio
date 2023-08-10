699b0377236cf392f3f3a39c353837fb19379689
Samson Cheung <samsonc89@yahoo.com>
2023-08-10 11:50:54 -0700

    Fix div order in new project card

--- src/components/projects/NewProjectCard.jsx
+++ src/components/projects/NewProjectCard.jsx
@@ -2,30 +2,33 @@
 import "./projectCard.css";
 import GitIcon from "../../assets/GitIcon";
 import LiveIcon from "../../assets/LiveIcon.jsx";
+import newImg from "../../../public/newBenefits.png";
 
 const NewProjectCard = ({ projectData: project }) => {
   return (
     <div className="new-project-card">
       <div className="new-project-thumbnail-container">
-        <img className="project-thumbnail" src={project.thumbnail} />
+        <img className="project-thumbnail" src={newImg} />
+        {/* <img className="project-thumbnail" src={project.thumbnail} /> */}
       </div>
       <div className="new-project-card-content-wrapper">
-        <div className="project-card-content">
+        <div className="new-project-card-content">
           <h2 className="new-project-title">{project.title}</h2>
           <p className="new-project-description">{project.description}</p>
         </div>
         <div className="project-card-bottom-wrapper">
           <div className="project-tech-wrapper"></div>
-          <div className="icon-container"></div>
-          <div className="icon-wrapper">
-            <a href={project.code} target="_blank" rel="noreferrer">
-              <GitIcon />
-            </a>
-          </div>
-          <div className="icon-wrapper">
-            <a href={project.live} target="_blank" rel="noreferrer">
-              <LiveIcon />
-            </a>
+          <div className="icon-container">
+            <div className="icon-wrapper">
+              <a href={project.code} target="_blank" rel="noreferrer">
+                <GitIcon />
+              </a>
+            </div>
+            <div className="icon-wrapper">
+              <a href={project.live} target="_blank" rel="noreferrer">
+                <LiveIcon />
+              </a>
+            </div>
           </div>
         </div>
       </div>
